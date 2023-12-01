ff642f386384f327f4c66377a92dc57b15a122a7
mlniza <mm.izza08@gmail.com>
2023-12-01 20:23:56 +0700

    add middleware

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const app = express();
 const fs = require("fs");
-
 const cors = require("cors");
-
 const PORT = 3000;
-
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 app.use(cors());
+
+const loggerMiddleware = (req, res, next) => {
+	const now = new Date();
+	const formattedTime = now.toLocaleDateString();
+	const method = req.method;
+	const url = req.url;
+	const status = res.statusCode;
+	console.log(`[${formattedTime}] ${method} ${url} - ${status}`);
+	next();
+};
+
+app.use(loggerMiddleware);
 
 app.get("/", function (req, res) 
 {
@@ -23,6 +34,40 @@ app.get("/destination", (req, res) =>
 		res.status(200).send(destination);
 	})
 
+})
+
+app.post("/destination", (req, res) => 
+{
+	console.log(req.body);
+	const { kota, provinsi, deskripsi} = req.body
+
+	fs.readFile("./data/destination.json", (err, data) => 
+	{
+		if (err) res.send("Gagal membaca json")
+		const destination = JSON.parse(data)
+		const newDestination = 
+		{
+			id: destination.length+1111,
+			kota: kota,
+			provinsi: provinsi,
+			deskripsi: deskripsi
+		}
+		destination.push(newDestination);
+
+		fs.writeFile
+		{
+			"./data/destination.json", JSON.stringify(destination, '', 2),
+			(err) => 
+			{
+				if (err) res.status(400).send("Gagal dalam memasukkan data");
+				res.status(201).send
+				({
+					message: "sukses dalam menambahkan data",
+					data: destination,
+				});
+			}
+		};
+	});
 })
 
 app.all("*", (req, res) =>
