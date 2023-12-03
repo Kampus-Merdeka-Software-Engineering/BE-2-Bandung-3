060561bf80f47709022b7b055469c166da5af199
mlniza <mm.izza08@gmail.com>
2023-12-03 21:13:15 +0700

    update index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const fs = require("fs");
 const cors = require("cors");
 const PORT = 3000;
+const {prisma} = require("./config/prisma")
+
+ 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors());
@@ -24,58 +27,30 @@ app.get("/", function (req, res)
 	response.send("Ini respon")
 });
 
-app.get("/destination", (req, res) => 
+app.get("/destination/id", async (req, res) => 
 {
-	// ambil data JSON dari /data/destination.JSON
-	fs.readFile('./data/destination.json', (error, data) => 
-	{
-		if (error) res.send("Gagal dalam pembacaan data")
-		const destination = JSON.parse(data)
-		res.status(200).send(destination);
-	})
-
-})
-
-app.post("/destination", (req, res) => 
-{
-	console.log(req.body);
-	const { kota, provinsi, deskripsi} = req.body
-
-	fs.readFile("./data/destination.json", (err, data) => 
-	{
-		if (err) res.send("Gagal membaca json")
-		const destination = JSON.parse(data)
-		const newDestination = 
+	const destination = await prisma.destination.findUnique({
+		where:
 		{
-			id: destination.length+1111,
-			kota: kota,
-			provinsi: provinsi,
-			deskripsi: deskripsi
-		}
-		destination.push(newDestination);
-
-		fs.writeFile
-		{
-			"./data/destination.json", JSON.stringify(destination, '', 2),
-			(err) => 
-			{
-				if (err) res.status(400).send("Gagal dalam memasukkan data");
-				res.status(201).send
-				({
-					message: "sukses dalam menambahkan data",
-					data: destination,
-				});
-			}
-		};
+			id: parseInt(req.params.id),
+		},
 	});
-})
+	if (!destination.length) res.status(404).send("destination");
+	else res.status(200).send(destination);
+});
+
+app.get("/destination", async (req, res) => 
+{
+	const destination = await prisma.destination.findMany();
+	res.status(200).send(destination)
+});
 
 app.all("*", (req, res) =>
 {
 	res.status(404).send("404 not found")
 })
 
-app.listen(PORT, () => 
+app.listen(PORT, "0.0.0.0", () => 
 {
     console.log(`server nyala cek di http://localhost:${PORT}`)
 })
